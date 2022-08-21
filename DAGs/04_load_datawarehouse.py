83031d1fe2b2af08d6b67e74c3ecc4800c205d12
Sandtw <sandtwice@gmail.com>
2022-08-21 01:30:47 -0500

    fourth

--- DAGs/04_load_datawarehouse.py
+++ DAGs/04_load_datawarehouse.py
@@ -1,4 +1,27 @@
+import pyspark
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 
-def dwh_dimenssions():
-    
+def create_spark_session():
+    spark = SparkSession.builder \
+        .master("yarn") \
+        .appName('dataproc-pyspark') \
+        .getOrCreate()
+    return spark
+
+def dwh_dimenssions(spark):
+    source = spark.read.format("parquet").option("header","true").option('inferSchema','true').load('/FileStore/tables/source/')
+    source = source.withColumn("population",col("population").cast("Double"))
+    energy = spark.read.format("parquet").option("header","true").option('inferSchema','true').load('/FileStore/tables/parq/')
+
+    # Hive
+    energy.createOrReplaceTempView("energytb")
+    source.createOrReplaceTempView("sourcetb")
+    table_poor = spark.sql("SELECT count(*) FROM sourcetb s join energytb e on s.country = e.country").show()
+
+def main():
+    spark = create_spark_session()
+    dwh_dimenssions(spark)
+
+if __name__ == "__main__":
+    main()
